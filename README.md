0ade14489d4812f8ffdc25ce3a390be34a94ace6
lgyn57 <axel.leguyen@gmail.com>
2023-01-28 23:22:13 +0100

    ajout mongo express et icon parking

---
+++ README.md
@@ -0,0 +1,17 @@
+Commandes:
+
+1# 
+
+docker-compose build
+
+2#
+docker-compose up
+
+3#
+docker exec my-container composer install /var/www/html
+
+
+rm -rf directoryname
+
+docker-compose exec --workdir / php /bin/bash
+
--- public/index.php
+++ public/index.php
@@ -3,37 +3,37 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // Code source permettant d'accéder aux données parking du Grand Nancy
-$pis = [];
+$parkings = [];
 
 $db = (new MongoDB\Client('mongodb://mongo'))->selectDatabase('tdmongo');
 $data = json_decode(file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson'));
-// $db->createCollection('pis');
-$db = $db->selectCollection('pis');
+$db->createCollection('parkings');
+$db = $db->selectCollection('parkings');
 
-// foreach ($data->features as $feature) {
-//   $pi = [
-//     'name' => $feature->attributes->NOM,
-//     'address' => $feature->attributes->ADRESSE,
-//     'description' => '',
-//     'category' => [
-//       'name' => 'parking',
-//       'icon' => 'fa-square-parking',
-//       'color' => 'blue'
-//     ],
-//     'geometry' => $feature->geometry,
-//     'places' => $feature->attributes->PLACES,
-//     'capacity' => $feature->attributes->CAPACITE,
-//   ];
-//   $pis[] = $pi;
-// }
-// if (count($pis) > 0) {
-//   $res = $db->insertMany($pis);
-// }
+foreach ($data->features as $feature) {
+  $parking = [
+    'name' => $feature->attributes->NOM,
+    'address' => $feature->attributes->ADRESSE,
+    'description' => '',
+    'category' => [
+      'name' => 'parking',
+      'icon' => 'fa-square-parking',
+      'color' => 'blue'
+    ],
+    'geometry' => $feature->geometry,
+    'places' => $feature->attributes->PLACES,
+    'capacity' => $feature->attributes->CAPACITE,
+  ];
+  $parkings[] = $parking;
+}
+if (count($parkings) > 0) {
+  $res = $db->insertMany($parkings);
+}
 
 $test = $db->find();
 
 foreach ($test as $t) {
-  $pis[] = $t;
+  $parkings[] = $t;
 
 }
 
@@ -66,14 +66,22 @@ foreach ($test as $t) {
 <body>
   <div id="map"></div>
   <script>
-    var map = L.map('map').setView([48.688, 6.186], 13);
+
+var parkingIcon = L.icon({
+    iconUrl: 'parking.png',
+    iconSize:     [53, 65], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+    var map = L.map('map').setView([48.688, 6.186], 15);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     var markers = L.markerClusterGroup();
-    <?php foreach ($pis as $pi) { ?>
-      var marker = L.marker([<?php echo $pi['geometry']->y; ?>, <?php echo $pi['geometry']->x; ?>]).bindPopup('<?php echo $pi['name']." - ".$pi['address']." - Places ".$pi['places']."/".$t['capacity']; ?>');
+    <?php foreach ($parkings as $parking) { ?>
+      var marker = L.marker([<?php echo $parking['geometry']->y; ?>, <?php echo $parking['geometry']->x; ?>], {icon: parkingIcon}).bindPopup('<?php echo $parking['name']." - ".$parking['address']." - Places ".$parking['places']."/".$t['capacity']; ?>');
       markers.addLayer(marker);
     <?php } ?>
     map.addLayer(markers);
